6701aac955b9ac0e0bd47fb71fd55e651b50f7db
Grzegorz Szymkowiak <szymkowiak.grzegorz@gmail.com>
2020-10-10 15:01:59 +0200

    Set up requests

---
+++ src/requests.js
@@ -0,0 +1,9 @@
+export const getShowsByKey = (key) => {
+    return fetch(`http://api.tvmaze.com/search/shows?q=${key}`).then(resp => resp.json());
+
+}
+
+export const getShowsById = (id) => {
+    return fetch(`http://api.tvmaze.com/shows/${id}?embed=cast`).then(resp => resp.json());
+
+}
